83ecf0df5a1a5f9abcaa19d85e99236b021fadd3
Nooptr <nooptr@techap.xyz>
2022-06-02 10:54:56 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# chrome-github-shortcut-extension
+
+## How to install the unpacked extension in Chrome
+
+```
+Step 1: Open chrome://extensions on browser
+Step 2: Enable developer mode
+Step 3: Click on Load Unpacked and select your folder.
+```
+
+## Shortcut
+
+Window: Ctrl + Shift + P
+
+Mac OS: Control + Shift + P
---
+++ background.js
@@ -0,0 +1,44 @@
+function copyToClipboard() {
+  var title = document.getElementsByClassName("gh-header-title")[0].innerText;
+  var description = document.getElementsByClassName("markdown-body")[0].innerText
+
+  var url = window.location.toString();
+
+  var text = title + "\n" + url + "\n" + description;
+
+  console.log("Copied");
+  console.log(text);
+
+  //Create a textbox field where we can insert text to.
+  var copyFrom = document.createElement("textarea");
+
+  //Set the text content to be the text you wished to copy.
+  copyFrom.textContent = text;
+
+  //Append the textbox field into the body as a child.
+  //"execCommand()" only works when there exists selected text, and the text is inside
+  //document.body (meaning the text is part of a valid rendered HTML element).
+  document.body.appendChild(copyFrom);
+
+  //Select all the text!
+  copyFrom.select();
+
+  //Execute command
+  document.execCommand('copy');
+
+  //(Optional) De-select the text using blur().
+  copyFrom.blur();
+
+  //Remove the textbox field from the document.body, so no other JavaScript nor
+  //other elements can get access to this.
+  document.body.removeChild(copyFrom);
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  if(!tab.url.includes("chrome://")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: copyToClipboard
+    });
+  }
+});
